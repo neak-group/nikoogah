d4393a82ad8098edf10206c806b02ad6275104c9
emsi-zero <emad.fdws@gmail.com>
2024-10-05 11:24:50 +0330

    update get participation usecase

--- server/internal/app/rally/rally/get_participants_usecase.go
+++ server/internal/app/rally/rally/get_participants_usecase.go
@@ -2,6 +2,7 @@ package rally
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/google/uuid"
 	"github.com/neak-group/nikoogah/internal/app"
@@ -12,20 +13,23 @@ import (
 
 type GetParticipantsUseCase struct {
 	app.BaseUseCase
-	repo                 repository.RallyRepository
-	rallyParticipationQS services.RallyParticipationQueryService
+	repo                        repository.RallyRepository
+	rallyParticipationQS        services.RallyParticipationQueryService
+	charityRepresentativeAccess services.CharityAccessService
 }
 
 type GetParticipantsUCParams struct {
 	app.UseCaseParams
-	Repo                 repository.RallyRepository
-	RallyParticipationQS services.RallyParticipationQueryService
+	Repo                        repository.RallyRepository
+	RallyParticipationQS        services.RallyParticipationQueryService
+	CharityRepresentativeAccess services.CharityAccessService
 }
 
 func ProvideGetParticipantsUC(params GetParticipantsUCParams) *GetParticipantsUseCase {
 	return &GetParticipantsUseCase{
-		repo:                 params.Repo,
-		rallyParticipationQS: params.RallyParticipationQS,
+		repo:                        params.Repo,
+		rallyParticipationQS:        params.RallyParticipationQS,
+		charityRepresentativeAccess: params.CharityRepresentativeAccess,
 		BaseUseCase: app.BaseUseCase{
 			Logger:          params.Logger,
 			EventDispatcher: params.EventDispatcher,
@@ -48,6 +52,14 @@ func (uc *GetParticipantsUseCase) Execute(ctx context.Context, params GetPartici
 	}
 
 	// TODO[Security]: Check Representative Access
+	hasAccess, err := uc.charityRepresentativeAccess.CanViewParticipation(ctx, rally.CharityID)
+	if err != nil {
+		return nil, err
+	}
+
+	if !hasAccess {
+		return nil, fmt.Errorf("use does not have access to this rally participation list")
+	}
 
 	participationList, err := uc.rallyParticipationQS.GetRallyHumanParticipation(ctx, rally.ID)
 	if err != nil {
